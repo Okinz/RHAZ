64154da37624850db85df804792915fb9050e718
janeth <jarri023@fiu.edu>
2020-02-01 13:11:45 -0500

    add moveto function

--- Assets/AlienMovement.cs
+++ Assets/AlienMovement.cs
@@ -3,7 +3,9 @@
 public class AlienMovement : MonoBehaviour
 {
   public Rigidbody2D rb;
-  private float thrust = 5.0f;
+  public Transform rocket;
+  public Transform position;
+  private float speed = 1.5f;
   // Start is called before the first frame update
   void Start()
   {
@@ -13,6 +15,9 @@ public class AlienMovement : MonoBehaviour
   // Update is called once per frame
   void FixedUpdate()
   {
-    rb.AddForce(Vector2.left * thrust);
+    float step = speed * Time.deltaTime;
+    //transform.position = Vector2.MoveTowards()
+    rb.AddForce(Vector2.MoveTowards() * speed);
+
   }
 }
