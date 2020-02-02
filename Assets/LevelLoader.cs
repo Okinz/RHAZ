bd74f05245446d34fc37798a6c329d3c923d474d
Okinz <steven14812@gmail.com>
2020-02-01 20:17:57 -0500

    Scene Transitions

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -8,17 +8,61 @@ public class LevelLoader : MonoBehaviour
     
     public Animator transition;
 
-    // Variable to adjust screen loading time
+    // // Variable to adjust screen loading time
     public float transitionTime = 1f;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetButtonDown("Vertical"))
         {
             LoadNextLevel();
         }
     }
+
+
+    
+
+    // public string sceneToLoad;
+
+    // void OnTriggerEnter2D(Collider2D col)
+    // {
+    //     if(col.CompareTag("Player"))
+    //     {
+    //         Debug.Log("Player Entered");
+    //         LoadNextLevel();
+    //         if(Input.GetKeyDown("e"))
+    //         {
+    //             Debug.Log("E is Pressed ENTER");
+    //             //SceneManager.LoadScene(sceneToLoad);
+    //             LoadNextLevel();
+    //             Debug.Log("After E is Pressed ENTER");
+    //         }
+    //     }
+    // }
+    // void OnTriggerStay2D(Collider2D col)
+    // {
+    //       if(col.CompareTag("Player"))
+    //     {
+    //         Debug.Log("Player Stayed");
+    //         LoadNextLevel();
+    //         if(Input.GetKeyDown("Vertical"))
+    //         {
+    //             Debug.Log("E is Pressed STAY");
+    //             //SceneManager.LoadScene(sceneToLoad);
+    //             LoadNextLevel();
+    //             Debug.Log("After E is Pressed STAY");
+    //         }
+    //     }
+    // }
+    // void OnTriggerExit2D(Collider2D col)
+    // {
+    //     if(col.CompareTag("Player"))
+    //         {
+    //             Debug.Log("EXIT 2D");
+    //         }
+    // }
+
 
 
     public void LoadNextLevel()
@@ -33,7 +77,8 @@ public class LevelLoader : MonoBehaviour
         yield return new WaitForSeconds(transitionTime);
 
         SceneManager.LoadScene(levelIndex);
-
-
     }
+
+
+
 }
